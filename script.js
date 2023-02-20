b2a3c7930996a13abbb0bdbaaaa4d0402152216a
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-20 01:16:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,77 @@
-  .tooltip {
-    position: absolute;
-    z-index: 1;
-    background-color: white;
-    border: 1px solid gray;
-    padding: 10px;
-    display: none;
-  }
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
-  .highlight {
-    fill: orange;
-  }
-</style>
+// create the SVG element
+var svg = d3.select("#my_bar_dataviz")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-<script>
-  const svg = d3.select("#bar-chart");
+// load the data and draw the chart
+d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-05-hw05-nick-ethan/master/data/bar-data.csv", function(data) {
 
-  const width = +svg.attr("width");
-  const height = +svg.attr("height");
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  // define the scales and axes
+  var x = d3.scaleBand()
+      .range([0, width])
+      .padding(0.2)
+      .domain(data.map(function(d) { return d.category; }));
 
-  const g = svg.append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+  var y = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, d3.max(data, function(d) { return d.amount; })]);
 
-  d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-05-hw05-nick-ethan/master/data/bar-data.csv")
-    .then(function(data) {
-      const x = d3.scaleBand()
-        .range([0, width - margin.left - margin.right])
-        .padding(0.2)
-        .domain(data.map(d => d.category));
+  var xAxis = d3.axisBottom(x);
+  var yAxis = d3.axisLeft(y);
 
-      const y = d3.scaleLinear()
-        .range([height - margin.top - margin.bottom, 0])
-        .domain([0, d3.max(data, d => +d.amount)]);
+  // add the x-axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)");
 
-      g.append("g")
-        .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-          .attr("transform", "translate(-10,0)rotate(-45)")
-          .style("text-anchor", "end");
+  // add the y-axis
+  svg.append("g")
+      .call(yAxis);
 
-      g.append("g")
-        .call(d3.axisLeft(y));
+  // add the bars
+  svg.selectAll(".bar")
+      .data(data)
+      .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.category); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.amount); })
+      .attr("height", function(d) { return height - y(d.amount); })
+      .on("mouseover", function(d) {
+        // change the fill color and display the tooltip
+        d3.select(this)
+          .attr("fill", "orange")
+          .append("title")
+          .text(function(d) { return d.amount; });
 
-      g.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-          .attr("class", "bar")
-          .attr("x", d => x(d.category))
-          .attr("y", d => y(+d.amount))
-          .attr("width", x.bandwidth())
-          .attr("height", d => height - margin.top - margin.bottom - y(+d.amount))
-          .on("mouseover", function(d) {
-            d3.select(this)
-              .classed("highlight", true)
-              .style("cursor", "pointer");
+        // display the tooltip
+        d3.select("#tooltip")
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px")
+          .html("Value: " + d.amount)
+          .style("display", "block");
+      })
+      .on("mouseout", function(d) {
+        // revert to the original fill color and hide the tooltip
+        d3.select(this)
+          .attr("fill", "blue")
+          .select("title")
+          .remove();
 
-            d3.select("#tooltip")
-              .style("left", d3.event.pageX + "px")
-              .style("top", d3.event.pageY + "px")
-              .style("display", "inline-block")
-              .html(`Value: ${d.amount}`);
-          })
-          .on("mousemove", function(d) {
-            d3.select("#tooltip")
-              .style("left", d3.event.pageX + "px")
-              .style("top", d3.event.pageY + "px");
-          })
-          .on("mouseout", function(d) {
-            d3.select(this)
-              .classed("highlight", false)
-              .style("cursor", "default");
-
-            d3.select("#tooltip")
-              .style("display", "none");
-          });
-    });
+        // hide the tooltip
+        d3.select("#tooltip")
+          .style("display", "none");
+      });
+});
