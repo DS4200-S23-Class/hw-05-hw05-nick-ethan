7aa8f375cfc230db1caf1b032cf6fe2a6540a93d
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-21 09:53:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-  const coordinates = document.getElementById("coordinates");
-  svg.selectAll('circle').on('click', function(event) {
-    const x = event.target.getAttribute('cx');
-    const y = event.target.getAttribute('cy');
-    coordinates.innerText = `(${x}, ${y})`;
-  });
+
