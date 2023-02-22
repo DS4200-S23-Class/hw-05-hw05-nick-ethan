ca4c3d1787d28a3fe753b7e4fc35ee7946632991
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-22 01:51:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,3 @@ function plot() {
   // get the x and y values from the dropdown menus
   var xValue = parseInt(document.getElementById("xdropdown").value);
   var yValue = parseInt(document.getElementById("ydropdown").value);
-
-  // add the point to the visualization
-  d3.select("#my_dataviz")
-    .append("circle")
-      .attr("cx", xValue * 50)
-      .attr("cy", yValue * 50)
-      .attr("r", 10)
-      .style("fill", "red");
-}
-
