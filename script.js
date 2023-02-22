58c8679a1fd8b686aece5c7993754f2857116c3d
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-22 01:53:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function plot() {
   // get the x and y values from the dropdown menus
   var xValue = parseInt(document.getElementById("xdropdown").value);
   var yValue = parseInt(document.getElementById("ydropdown").value);
+  var svg = d3.select("#my_dataviz");
+  svg.append("circle")
+    .attr("cx", x * 50) // assuming a scale of 50 units per x/y value
+    .attr("cy", y * 50)
+    .attr("r", 5)
+    .style("fill", "red")
+    .style("stroke", "black");
+}
